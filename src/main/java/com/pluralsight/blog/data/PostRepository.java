9c7876cb5e68d12da9fb14bada25ee8691740946
AlexT <alexandertian82@gmail.com>
2020-12-22 18:26:18 +0100

    commi2

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,4 +4,8 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByTitleContaining(String title);
+
+}
