874f622367eb4ce949ea45b5cb79348add2b58f1
AlexT <alexandertian82@gmail.com>
2020-12-23 17:22:40 +0100

    6

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,9 +3,9 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @RestResource(rel="contains-title", path="containsTitle")
-    List<Post> findByTitleContaining(String title);
-
+    public List<Post> findByTitleContaining(String title);
 }
